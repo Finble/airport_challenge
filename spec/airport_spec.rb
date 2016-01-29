751c108cebe67e7e64310bbe16c5ee7a523f53c6
Finble <elainefinn@hotmail.com>
2016-01-29 12:13:52 +0000

    Refactored tests + code

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,22 +3,34 @@ require 'airport'
 describe Airport do
 
 	subject(:airport) {described_class.new(20)}  # subject = new class of described class, which is Airport
+	let(:plane) {double (:plane)}  #declare variable at top of tests, vs within each test
 
-	it 'instructs a plane to land' do
-		expect(subject).to respond_to(:land).with(1).argument  #unsure how these 3 lines are shorter than a 1 liner: it {is_expected.to respond_to(:land).with(1).argument}
-	end
+	describe '#land' do
 
-	it 'instructs a plane to take off' do
-		expect(subject).to respond_to(:take_off)
-	end
-
-	it 'does not allow landing when at capacity' do
-		plane = double(:plane)
-		20.times do
-			airport.land(plane)
+		it 'instructs a plane to land' do
+			expect(subject).to respond_to(:land).with(1).argument  #unsure how these 3 lines are shorter than a 1 liner: it {is_expected.to respond_to(:land).with(1).argument}
 		end
-		expect{airport.land(plane)}.to raise_error 'Cannot land plane: airport full' 
+
+		context 'when full' do
+
+			it 'raises an error' do
+				20.times do
+					airport.land(plane)
+				end
+				expect{airport.land(plane)}.to raise_error 'Cannot land plane: airport full' 
+			end
+		end
+
 	end
+
+	describe '#take off' do
+
+		it 'instructs a plane to take off' do
+			expect(subject).to respond_to(:take_off)
+		end
+
+	end
+
 end
 	
 ## ?? ## why did we using :plane (vs plane) here, as we do in user_stories_spec (before refactoring to a double)?  Plane is a variable, not a method (or double for subject)?
