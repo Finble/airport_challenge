90728efe7f28417752075e14047931e850982d2a
Finble <elainefinn@hotmail.com>
2016-01-29 12:08:12 +0000

    User story 3 - prevent landing if airport full + plane array (Exemplar user story version)

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,17 +2,24 @@ require 'airport'
 
 describe Airport do
 
-	subject(:airport) {described_class.new}  # subject = new class of described class, which is Airport
+	subject(:airport) {described_class.new(20)}  # subject = new class of described class, which is Airport
 
 	it 'instructs a plane to land' do
 		expect(subject).to respond_to(:land).with(1).argument  #unsure how these 3 lines are shorter than a 1 liner: it {is_expected.to respond_to(:land).with(1).argument}
 	end
 
-	it 'instrcuts a plane to take off' do
+	it 'instructs a plane to take off' do
 		expect(subject).to respond_to(:take_off)
 	end
 
+	it 'does not allow landing when at capacity' do
+		plane = double(:plane)
+		20.times do
+			airport.land(plane)
+		end
+		expect{airport.land(plane)}.to raise_error 'Cannot land plane: airport full' 
+	end
 end
 	
-
+## ?? ## why did we using :plane (vs plane) here, as we do in user_stories_spec (before refactoring to a double)?  Plane is a variable, not a method (or double for subject)?
 
--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -2,25 +2,51 @@ require 'airport'
 
 describe 'These are all the user stories' do
 
+# User Story 1
+##############
 # As an air traffic controller 
 # So I can get passengers to a destination 
 # I want to instruct a plane to land #(at an airport and confirm that it has landed)
 
-	it 'so planes land at airport, instruct a plane to land'  do
-		airport = Airport.new
+	it 'so planes land at airport, instruct a plane to land' do
+		airport = Airport.new(20)
 		plane = Plane.new
 		expect{airport.land(plane)}.not_to raise_error #null test
 	end
 
+# User Story 2
+##############
 # As an air traffic controller 
-# So planes can take off safely from my airport"
-# I would like to instruct a plane to take off"
+# So planes can take off safely from my airport
+# I would like to instruct a plane to take off
+
+## ?? ## I want to code 'instruct - by whom to whom' and 'safely - some measure' and 'from an airport'...!!
 
 	it 'so planes take off from airports, instruct a plane to take off' do
-		airport = Airport.new
+		airport = Airport.new(20)
 		plane = Plane.new
 		expect{airport.take_off(plane)}.not_to raise_error 
 	end
+
+# User Story 3
+##############
+# As an air traffic controller 
+# So I can avoid collisions
+# I would to prevent airplanes landing when my airport is full
+
+	it 'does not allow planes to land when airport is full' do
+		airport = Airport.new(20)
+		plane = Plane.new
+		20.times do
+			airport.land(plane)
+		end
+		expect{airport.land(plane)}.to raise_error 'Cannot land plane: airport full' 
+	end
 end
 
-## ?? ## I want to code 'instruct - by whom to whom' and 'safely - some measure' and 'from an airport'...!!
+## ?? ## at point args failed, I would have fixed by using a constant (wtih a capacity value) vs updating all new instances (and methods using new instances) with (20)!
+
+
+
+
+
